7bd92ebb582d76d07b1a1688514df391bb71fd8b
Romeo Folie <romeofolie1@gmail.com>
2018-07-25 19:16:12 +0000

    Added hbs template for main page

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const port = process.env.PORT || 3000
 
 const app = express()
 app.set('view engine', 'hbs')
-app.use(express.static(__dirname +'/public'))
+app.use(express.static('public'))
 
-
+app.get('/', (req, res) => {
+  res.render('index.hbs',{title: "Home"})
+})
 
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
