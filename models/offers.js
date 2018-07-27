b4c6a490f83b94794556a38bf49ccdb1a849629a
Romeo Folie <romeofolie1@gmail.com>
2018-07-27 00:43:38 +0000

    set up schema for Offer collection

---
+++ models/offers.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+var OfferSchema = new mongoose.Schema({
+  title:{
+    type: String,
+    required: true,
+    trim: true
+  },
+  link:{
+    type: String,
+    required: true,
+    trim: true,
+    unique, true
+  },
+  excerpt:{
+    type: String,
+    trim: true
+  },
+  location:{
+    type: String,
+    trim: true,
+  }
+});
+
+var Offer = mongoose.model('Offer', OfferSchema)
+
+module.exports = {Offer}
