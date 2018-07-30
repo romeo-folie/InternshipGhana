6bc86401d053c4b3051c13dc9cc8f615a2559616
Romeo Folie <romeofolie1@gmail.com>
2018-07-30 15:32:43 +0000

    modified production environment variables

--- config/config.js
+++ config/config.js
@@ -1,12 +1,23 @@
 var env = process.env.NODE_ENV || 'development'
 
+const passValues = () => {
+  return Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key]
+  })
+}
+
 if (env === 'development'){
   var config = require('./config.json');
   var envConfig = config[env]
-  //for each of the keys inside the envConfig object process.env.key is the value of the envConfig object's value for that same key
-  Object.keys(envConfig).forEach((key) => {
-    process.env[key] = envConfig[key]
-  });
+  passValues()
+} else if(env === 'production'){
+  var config = require('./config.json');
+  var envConfig = config[env]
+  passValues()
 }
 
 //"mongodb://localhost:27017/InternshipApp"
+
+// Object.keys(envConfig).forEach((key) => {
+//   process.env[key] = envConfig[key]
+// });
